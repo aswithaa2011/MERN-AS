8d5997ecd6a1823bc83c1556efccff24311c5168
aswithaa2011 <160911533+aswithaa2011@users.noreply.github.com>
2026-03-01 20:24:44 +0530

    Assignment day 8

---
+++ JS/Day 8/Task.js
@@ -0,0 +1,74 @@
+
+// for ( let i=1;i<=5;i++){
+//     let res=""
+//     for ( let j=1;j<=5;j++){
+//         res+="*"
+//     }
+//     console.log(res);
+    
+// }
+
+// Reverse a string (without built-in)
+
+// let str="Hello";
+// let rev=""
+
+// for(let i=str.length-1;i>=0;i--){
+//      rev+=str[i]
+
+// }
+
+// console.log(rev);
+
+// //
+
+
+
+// a palindrome 
+
+// let str="malayalam";
+// let rev=""
+
+// for(let i=str.length-1;i>=0;i--){
+//      rev+=str[i]
+
+// }
+
+// if(str===rev){
+//     console.log(str ,"is a Palindrome");
+    
+
+
+// }
+
+// else{
+//     console.log(str ,"not a palindrome");
+    
+// }
+
+
+//factoorial
+
+// const number=5;
+
+// let fact=1;
+
+// for(let i=1;i<=number;i++){
+//     fact=fact*i
+// }
+
+// console.log(`Factorial of ${number} is ${fact}`);
+
+//fibonnacci series
+
+// let a = 0;
+// let b = 1;
+// let next;
+
+// for (let i = 1; i <= 10; i++) {
+//     console.log(a);
+
+//     next = a + b;
+//     a = b;
+//     b = next;
+// }
